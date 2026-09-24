527cf42f2b81bea8bc7aafd79b295b61b73d48bd
Engenheiro Muniz <engenheiro.muniz@outlook.com>
2026-09-24 14:41:08 -0300

    Bug proposital

--- src/main/java/com/amztecnologia/cicd_facil/service/SaudacaoService.java
+++ src/main/java/com/amztecnologia/cicd_facil/service/SaudacaoService.java
@@ -7,7 +7,7 @@ public class SaudacaoService {
 	
 	public String saudar(String nome) {
 		if(nome == null || nome.isBlank()) {
-			throw new IllegalArgumentException("O nome é obrigatório.");
+			throw new IllegalArgumentException("ATENÇÃO - O nome é obrigatório.");
 		}
 		
 		return "Identificação confirmada. "+nome.trim()+" você está dentro de uma operação CI/CD";		
